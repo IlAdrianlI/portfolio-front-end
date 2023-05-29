05915dbeef269f06409207d8f9d28276bc1a0f8d
Adrian Albuquerque <adrian.albuquerque@outlook.com.br>
2023-05-29 16:46:55 -0300

    menu criado

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,44 @@
 import './Header.css';
+import Menu from "./images/menu.svg";
 
 function Header() {
   return (
-    <div className="Header">
-        <h2>Header</h2>
-    </div>
+
+    <header>
+        <div className="nav-bar">
+            
+            <header className='logo'>
+                <h1>Logo</h1>
+            </header>
+
+            <div className='nav-list'>
+                <ul>
+                    <li className='nav-item'><a className='nav-link' href='#'>Inicío</a></li>
+                    <li className='nav-item'><a className='nav-link' href='#'>Sobre mim</a></li>
+                    <li className='nav-item'><a className='nav-link' href='#'>Projetos</a></li>
+                    <li className='nav-item'><a className='nav-link' href='#'>Habilidades</a></li>
+                </ul>
+            </div>{/*nav-list*/}
+
+            <div className='menu-mobile-icon'>
+                <button><img className='icon' src={Menu} id='menu'/></button>
+            </div>
+
+        </div>{/*nav-bar*/}
+
+        <div className='menu-mobile'>
+            <ul>
+                <li className='nav-item'><a className='nav-link' href='#'>Inicío</a></li>
+                <li className='nav-item'><a className='nav-link' href='#'>Sobre mim</a></li>
+                <li className='nav-item'><a className='nav-link' href='#'>Projetos</a></li>
+                <li className='nav-item'><a className='nav-link' href='#'>Habilidades</a></li>
+            </ul>
+        </div>
+
+    </header>
   );
+
 }
 
 export default Header;
+
